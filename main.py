772845d873d48f915a0499f0192e0c6867757ca3
Jeet <jeetthakkar648@gmail.com>
2025-09-16 18:12:11 +0530

    Multiply

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ def Addition(a,b):
     return(a+b)
 
 def Sub(a,b):
-    return()
+    return(a-b)
 
 def multiply(a,b):
-    return()
+    return(a*b)
 
 a=int(input("Enter the first number"))
 b=int(input("Enter the second number"))
